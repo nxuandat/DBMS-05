54b50a6274292a3e1595c5beb9bd9bbe90107f0e
huy194 <57141767+huy194@users.noreply.github.com>
2023-12-14 15:08:46 +0700

    Update LogIn.tsx

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -71,8 +71,9 @@ export default function LogIn() {
     >
       <div className='login-container d-flex align-items-center justify-content-center py-5 h-100'>
         <div className='row d-flex align-items-center justify-content-center h-80 w-80'>
+          <div className='col-md-8 col-lg-7 col-xl-6'>
           <img src={pictureLogin} className='img-fluid' alt='Phone image' />
-        </div>
+          </div>
         <div className='col-md-7 col-lg-5 col-xl-5 offset-xl-1'>
           <h1>ĐĂNG NHẬP</h1>
           <form onSubmit={handleLogin}>
@@ -149,6 +150,7 @@ export default function LogIn() {
           <ToastContainer />
         </div>
       </div>
+    </div>
     </div>
   );
 }
